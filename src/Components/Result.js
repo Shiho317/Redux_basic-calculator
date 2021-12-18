6761d4f6ce4ef7f85efac5a487b7696abdefcd1c
Shiho Nagano <shiho@nagano.com>
2021-12-18 18:50:44 +0900

    complete calculator

---
+++ src/Components/Result.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Result({result}) {
+  return (
+    <div className='resultValue'>
+      {result}
+    </div>
+  )
+}
+
+export default Result
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './Redux/reducers';
+
+
+const Store = createStore(reducer);
 
 ReactDOM.render(
+  <Provider store={Store}>
     <App />
-  ,
+  </Provider>,
   document.getElementById('root')
 );
 
